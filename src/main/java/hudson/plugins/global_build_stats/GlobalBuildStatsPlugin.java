5c38af02b11d66eb722647fbad918a5897550ed4
Tom Janson <tom.janson@lancom.de>
2019-08-13 14:06:04 +0200

    rm unused imports

--- src/main/java/hudson/plugins/global_build_stats/GlobalBuildStatsPlugin.java
+++ src/main/java/hudson/plugins/global_build_stats/GlobalBuildStatsPlugin.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
